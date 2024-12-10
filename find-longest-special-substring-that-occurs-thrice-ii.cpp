a695d6660dded8c65adfe129cbc8c5b3e1a0d14a
Ashish Khurana <114246997+ashish2125it@users.noreply.github.com>
2024-12-10 14:17:16 +0530

    Add find-longest-special-substring-that-occurs-thrice-ii.cpp

---
+++ find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool solve(int len, vector<int>& nums, int n) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += max(0, nums[i] - len + 1);
+        }
+        return cnt >= 3;
+    }
+
+    int maximumLength(string s) {
+        int n = s.length();
+        vector<vector<int>> v(26); 
+        int i = 0;
+        while (i < n) {
+            int cnt = 1;
+            while (i + 1 < n && s[i] == s[i + 1]) {
+                i++;
+                cnt++;
+            }
+            v[s[i] - 'a'].push_back(cnt);
+            i++;
+        }
+
+        int ans = -1;
+        for (int i = 0; i < 26; i++) {
+            vector<int> temp = v[i];
+            if (temp.empty()) continue; 
+            sort(temp.begin(), temp.end());
+            int low = 1, high = temp.back(); 
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (solve(mid, temp, temp.size())) {
+                    ans = max(ans, mid);
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
