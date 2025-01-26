78ea0799549924f32a042e2fa1bd6b0cbdd40e21
Ashish Khurana <114246997+ashish2125it@users.noreply.github.com>
2025-01-27 02:27:07 +0530

    Add maximum-subarray.cpp

---
+++ maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int n = nums.size();
+    int sum =0 ,ans= INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        sum +=  nums[i];
+       
+        ans =max(ans,sum);
+        if(sum<0)
+        sum =0;
+    }
+    return ans;
+    }
+};
